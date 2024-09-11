bac70fbd858accf14714624ec0aff2f5122a39fa
fq <q453317366@163.com>
2024-09-11 18:24:22 +0800

    框架搭建及返回类Result的编写

--- FQ-framework/src/main/java/com/fqishappy/service/ArticleService.java
+++ FQ-framework/src/main/java/com/fqishappy/service/ArticleService.java
@@ -1,6 +1,7 @@
 package com.fqishappy.service;
 
 import com.baomidou.mybatisplus.extension.service.IService;
+import com.fqishappy.domain.ResponseResult;
 import com.fqishappy.domain.entity.Article;
 
 /**
@@ -8,5 +9,10 @@ import com.fqishappy.domain.entity.Article;
  * @date 2024/9/10 18:38
  */
 public interface ArticleService extends IService<Article> {
+    /**
+     * 返回热门文章
+     * @return
+     */
+    ResponseResult hotArticleList();
 }
 
